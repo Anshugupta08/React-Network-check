18aeab4d84d61b4e7e9b8b72f3e73499ef2582cf
Anshugupta08 <gomtinagar08@gmail.com>
2024-02-02 19:44:00 +0530

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
-import "./index.css";
+// import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-import NetworksStatus from "./NetworkStatus/ckecknetwork.jsx";
+import NetworksStatus from "./Networkstatus/ckecknetwork";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
